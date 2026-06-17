767964c34577bc598cb7545b37415d6978f5c842
daro128 <p8735036@gmail.com>
2026-06-17 13:41:13 +0700

    backend test login registers

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,48 @@
 import express from "express";
-import cors from "cors";
+import pool from "./src/config/db.js";
+import {findbyemail,createcustomer} from "./repositories/user_repositories.js"
 
-const app=express();
+import dotenv from "dotenv"
+dotenv.config({path:"../.env"})
+import authRoutes from "./route/auth_route.js"
+// console.log(process.env.DB_HOST)
+const app = express();
+const PORT = 3000;
 
-app.use(cors());
+app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.json({
-    users: ["user1", "user2", "user3"]
-  });
-});
+// async function testConnection() {
+//   try {
+//     const connection = await pool.getConnection();
+//     console.log("Database Connected");
+//     connection.release();
+//   } catch (error) {
+//     console.error("Database Connection Failed");
+//     console.error(error.message);
+//   }
+// }
+// testConnection();
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+// async function test() {
+//     try{
+//         const user= await findbyemail("daro128@gmail.com")
+//         console.log(user)
+//     }catch(error){
+//         console.log(error.message)
+//     }
+// }
+// async function test1() {
+//     try{
+//         const user= await createcustomer("Rat","daro","daro128@gmail.com","122233455","123456")
+//         console.log(user)
+//     }catch(error){
+//         console.log(error.message)
+//     }
+// }
+
+// test()
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
