d686e06dd954f66339d03646d84c87507cef538a
PSoth911 <puthpissoth.seu@student.cadt.edu.kh>
2026-06-17 13:50:26 +0700

    update merge

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
 import cors from "cors";
 import { pool } from "./database/db.js";
 
@@ -68,50 +67,7 @@ app.post("/api/admin/login", async (req, res) => {
     },
   });
 });
-=======
-import pool from "./src/config/db.js";
-import {findbyemail,createcustomer} from "./repositories/user_repositories.js"
 
-import dotenv from "dotenv"
-dotenv.config({path:"../.env"})
-import authRoutes from "./route/auth_route.js"
-// console.log(process.env.DB_HOST)
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-// async function testConnection() {
-//   try {
-//     const connection = await pool.getConnection();
-//     console.log("Database Connected");
-//     connection.release();
-//   } catch (error) {
-//     console.error("Database Connection Failed");
-//     console.error(error.message);
-//   }
-// }
-// testConnection();
->>>>>>> origin/daro
-
-// async function test() {
-//     try{
-//         const user= await findbyemail("daro128@gmail.com")
-//         console.log(user)
-//     }catch(error){
-//         console.log(error.message)
-//     }
-// }
-// async function test1() {
-//     try{
-//         const user= await createcustomer("Rat","daro","daro128@gmail.com","122233455","123456")
-//         console.log(user)
-//     }catch(error){
-//         console.log(error.message)
-//     }
-// }
-
-// test()
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
